7362010812895d6f005d1bbe8903775711d35323
vitroid <vitroid@gmail.com>
2023-12-27 21:18:15 +0900

    Python化(とりあえず)

--- unitcell.py
+++ unitcell.py
@@ -1,8 +1,4 @@
-# Google Bardによる翻訳 (かなりおかしいけど)
-import math
-
-
-def push(jx, jy, jz, t, r):
+def push(stack, jx, jy, jz, t, rj):
     """
     既存のベクトルと素かどうかをチェックしろ
 
@@ -14,49 +10,50 @@ def push(jx, jy, jz, t, r):
       r: 長さ
     """
 
-    for i in range(nstack):
-        if jx * stackx[i] + jy * stacky[i] + jz * stackz[i] == 0:
+    nj = jx * jx + jy * jy + jz * jz
+    for i in range(len(stack)):
+        ix, iy, iz, _, _ = stack[i]
+        ni = ix * ix + iy * iy + iz * iz
+        nij = ix * jx + iy * jy + iz * jz
+        if nij * nij == ni * nj:
+            if nj < ni:
+                stack[i] = jx, jy, jz, t, rj
             return
 
-    stackx.append(jx)
-    stacky.append(jy)
-    stackz.append(jz)
-    stackt.append(t)
-    stackr.append(r)
-    nstack += 1
+    stack.append((jx, jy, jz, t, rj))
 
 
 def main():
     bx, by, bz = 1.0, 1.0, 1.0
     ix, iy, iz = 1, 1, 1
-    range = 10
+    grid_range = 10
     tolerance = 0.02
-    ri = math.sqrt(ix**2 + iy**2 + iz**2)
+    ri = (ix**2 + iy**2 + iz**2) ** 0.5
 
-    for jx in range(-range, range + 1):
-        for jy in range(-range, range + 1):
-            for jz in range(-range, range + 1):
+    stack = []
+
+    for jx in range(-grid_range, grid_range + 1):
+        for jy in range(-grid_range, grid_range + 1):
+            for jz in range(-grid_range, grid_range + 1):
                 rjx = jx * bx
                 rjy = jy * by
                 rjz = jz * bz
-                rj = math.sqrt(rjx**2 + rjy**2 + rjz**2)
+                rj = (rjx**2 + rjy**2 + rjz**2) ** 0.5
                 if rj != 0:
                     tj = abs(ix * rjx + iy * rjy + iz * rjz) / (ri * rj)
                     if tj < tolerance:
-                        push(jx, jy, jz, tj, rj)
+                        push(stack, jx, jy, jz, tj, rj)
 
-    for j in range(nstack):
-        rjx = stackx[j] * bx
-        rjy = stacky[j] * by
-        rjz = stackz[j] * bz
-        tj = stackt[j]
-        rj = stackr[j]
-        for k in range(j + 1, nstack):
-            rkx = stackx[k] * bx
-            rky = stacky[k] * by
-            rkz = stackz[k] * bz
-            tk = stackt[k]
-            rk = stackr[k]
+    for j in range(len(stack)):
+        xj, yj, zj, tj, rj = stack[j]
+        rjx = xj * bx
+        rjy = yj * by
+        rjz = zj * bz
+        for k in range(j + 1, len(stack)):
+            xk, yk, zk, tk, rk = stack[k]
+            rkx = xk * bx
+            rky = yk * by
+            rkz = zk * bz
             tjk = abs(rkx * rjx + rky * rjy + rkz * rjz) / (rk * rj)
             if tjk < tolerance:
                 x = rky * rjz - rkz * rjy
@@ -64,17 +61,17 @@ def main():
                 z = rkx * rjy - rky * rjx
                 v = abs(x * ix + y * iy + z * iz)
                 print(
-                    int(math.rint(v / (bx * by * bz))),
-                    v / (ri * rj * rk),
+                    int(v / (bx * by * bz)),
+                    f"{v / (ri * rj * rk):.6f}",
                     ix,
                     iy,
                     iz,
-                    stackx[j],
-                    stacky[j],
-                    stackz[j],
-                    stackx[k],
-                    stacky[k],
-                    stackz[k],
+                    xj,
+                    yj,
+                    zj,
+                    xk,
+                    yk,
+                    zk,
                 )
 
 
